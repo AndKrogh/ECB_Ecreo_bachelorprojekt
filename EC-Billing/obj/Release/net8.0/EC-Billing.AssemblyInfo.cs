47159d44d0a76023ffae088ea4ef727b5bece096
AndKrogh <anders.krogh99@gmail.com>
2024-10-25 16:34:59 +0200

    formatted file locally

--- EC-Billing/obj/Release/net8.0/EC-Billing.AssemblyInfo.cs
+++ EC-Billing/obj/Release/net8.0/EC-Billing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EC-Billing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c802888fc127f2bac217db7179f92df3ec75fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52915fb96517d2c245401885c69f5996d671fb86")]
 [assembly: System.Reflection.AssemblyProductAttribute("EC-Billing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EC-Billing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
