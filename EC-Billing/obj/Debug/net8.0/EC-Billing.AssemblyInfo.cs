20327a08351174c014202db68ad04282f4c761f1
AndKrogh <anders.krogh99@gmail.com>
2024-12-10 21:53:29 +0100

    adding dbcontext

--- EC-Billing/obj/Debug/net8.0/EC-Billing.AssemblyInfo.cs
+++ EC-Billing/obj/Debug/net8.0/EC-Billing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EC-Billing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef30f7b8261be83e4af713b38f693acfae007a3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8bfca04c241fd916a25da77b4119329cd1e1835")]
 [assembly: System.Reflection.AssemblyProductAttribute("EC-Billing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EC-Billing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
