9cc72c62cf2b0dae6685048611c6cf2151e55d7b
AndKrogh <anders.krogh99@gmail.com>
2024-12-10 21:57:11 +0100

    adding employee.cs

--- EC-Billing/obj/Debug/net8.0/EC-Billing.AssemblyInfo.cs
+++ EC-Billing/obj/Debug/net8.0/EC-Billing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EC-Billing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8bfca04c241fd916a25da77b4119329cd1e1835")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20327a08351174c014202db68ad04282f4c761f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EC-Billing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EC-Billing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
